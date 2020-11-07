b0e87005bff9ff7403413aa4a2ca302acc62341f
Simon Lickus <darbolickus@gmail.com>
2020-11-07 23:30:11 +0200

    added some comments for myself

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@
 import Phaser from 'phaser';
 
 export default {
-  type: Phaser.AUTO,
-  parent: 'game',
+  type: Phaser.AUTO, // auto picks webgl if its possible
+  parent: 'game', // name of the div in html file
   backgroundColor: '#33A5E7',
   scale: {
     width: 800,
     height: 600,
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.FIT, // Phaser tries automatically scalling canvas to fit the screen. For example zooming in. But its not changing 800x600
+    autoCenter: Phaser.Scale.CENTER_BOTH, // centers canvas
   },
 };
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,16 +6,22 @@ class Game extends Phaser.Scene {
     super({ key: 'GameScene' });
   }
 
+  // this will be auto used by Phaser when Phaser starts the scene. So you can pass some settings here.
   init(data) {}
 
+  // place to preolad all the image assets.
+  // 'logo' is the key used later in create method
   preload() {
     this.load.image('logo', 'assets/phaser3-logo.png');
   }
 
+  // after preload method this one getting called.
+  // this one is creating new Game object and in this case using its add method. 
   create(data) {
+    // location and image key from preload method
     this.add.image(400, 300, 'logo');
   }
-
+  // tick method, runs 60fps.
   update(time, delta) {}
 }
 
